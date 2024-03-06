9ca5971656a6ea0b6a383194f46601b0212434d5
Sunghyun hwang <sunghyunzz@banksalad.com>
2024-03-06 10:39:00 +0900

    Add asdf
    Signed-off-by: Sunghyun hwang <sunghyunzz@banksalad.com>

---
+++ asdf.sh
@@ -0,0 +1,24 @@
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf list all nodejs
+read -p "🧞 Node.js version: " nodejsversion
+asdf install nodejs $nodejsversion
+asdf global nodejs $nodejsversion
+
+asdf plugin-add pnpm
+asdf list all pnpm
+read -p "🧞 pnpm version: " pnpmversion
+asdf install pnpm $pnpmversion
+asdf global pnpm $pnpmversion
+
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf list all golang
+read -p "🧞 Go version: " goversion
+asdf install golang $goversion
+asdf global golang $goversion
+. ~/.asdf/plugins/golang/set-env.zsh
+
+asdf plugin-add python
+asdf list all python
+read -p "🧞 Python version: " pythonversion
+asdf install python $pythonversion
+asdf global python $pythonversion
--- brew.sh
+++ brew.sh
@@ -5,8 +5,8 @@ brew tap homebrew/services
 brew tap homebrew/cask-fonts
 
 brew reinstall git
-brew install asdf bat curlie eza fd fzf procs tree wget zoxide 
+brew install asdf bat coreutils curl curlie eza fd fzf procs tree wget zoxide 
 
 brew install --cask 1password google-chrome hey
 brew install --cask notion notion-calendar figma
-brew install --cask raycast iterm2 tower visual-studio-code jetbrains-toolbox
+brew install --cask raycast warp tower visual-studio-code jetbrains-toolbox
