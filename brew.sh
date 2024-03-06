470ff1a059ada7b9b6131624288779262dbbf56b
Sunghyun hwang <sunghyunzz@banksalad.com>
2024-03-06 10:42:31 +0900

    Add fonts
    Signed-off-by: Sunghyun hwang <sunghyunzz@banksalad.com>

--- brew.sh
+++ brew.sh
@@ -10,3 +10,4 @@ brew install asdf bat coreutils curl curlie eza fd fzf procs tree wget zoxide
 brew install --cask 1password google-chrome hey
 brew install --cask notion notion-calendar figma
 brew install --cask raycast warp tower visual-studio-code jetbrains-toolbox
+brew install --cask font-fira-code font-jetbrains-mono font-hack font-source-code-pro font-inconsolata font-cascadia-code 
