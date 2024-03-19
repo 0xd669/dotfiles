3e128550b382916717bf2f3f02bc5b20e42d3c29
Sunghyun hwang <sunghyunzz@banksalad.com>
2024-03-19 13:07:20 +0900

    Update
    Signed-off-by: Sunghyun hwang <sunghyunzz@banksalad.com>

--- brew.sh
+++ brew.sh
@@ -11,6 +11,7 @@ brew reinstall git
 brew install asdf bat coreutils curl curlie eza fd fzf procs tree wget zoxide 
 
 brew install --cask 1password google-chrome hey
-brew install --cask notion notion-calendar figma
-brew install --cask raycast warp tower visual-studio-code jetbrains-toolbox
+brew install --cask notion notion-calendar figma shottr
+brew install --cask raycast warp tower visual-studio-code
+brew install --cask docker
 brew install --cask font-fira-code font-jetbrains-mono font-hack font-source-code-pro font-inconsolata font-cascadia-code 
