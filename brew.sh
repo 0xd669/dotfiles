9c95982289bd076f734f81494d8d6c4149f0295d
0xd669 <0xd669@gmail.com>
2025-03-17 11:27:00 +0900

    Add apps

--- brew.sh
+++ brew.sh
@@ -10,6 +10,8 @@ brew tap homebrew/cask-fonts
 brew reinstall git
 brew install asdf bat coreutils curl curlie eza fd fzf procs tree wget zoxide 
 
-brew install --cask figma shottr
-brew install --cask orbstack raycast tower warp
+brew install --cask raycast shottr
+brew install --cask 1password arc
+brew install --cask orbstack tower warp cursor
+brew install --cask figma
 brew install --cask font-fira-code font-jetbrains-mono font-hack font-source-code-pro font-inconsolata font-cascadia-code 
